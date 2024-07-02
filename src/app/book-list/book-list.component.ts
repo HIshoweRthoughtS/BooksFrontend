954edbe19c9e9e6bd93318e9aa336a9841edddad
HIschoweRthoughtS <jonasprivat@tutamail.com>
2024-07-02 14:56:08 +0200

    swap content to match component names

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,50 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { MemoryDbService } from '../shared/services/memory-db.service';
-import { Observable } from 'rxjs';
-import { Book } from '../shared/interfaces';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-book-list',
+  selector: 'app-review-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
 })
-export class BookListComponent implements OnInit {
-
-  books$:Observable<Book[]>;
-
-  //I need a booklist service or ngrx/store
-  //Links to Short Essay, maybe with a comment section
-  //links to authors, dates, and other extra info
-  //possibility to add books to the list with all the
-  //neccessary info filled out. a first created date is
-  //created and fieds for future date updates will be summoned
-  
-  //todo: later a backend with the needed info
-  //todo: backend has endpoints for every read and the SmallBook info list.
-  //frontend pieces together all info. loading every review/read when needed.
-  //e.g. select a read from booklist -> show this read and review
-  constructor(private dbService:MemoryDbService) {
-    this.books$ = dbService.Books;
-  }
-
-  ngOnInit(): void {
-
-  }
-  //i will work in reads. that means, everytime i read a book i can start a new read
-  //-> the book will be marked as in progress (currently reading)
-  //later i can press a button: finfished
-  //then i will be redirected to fill out a review form with the known properties
-  //so a review has to be connected to a "read". the read properties can of course be faked.
-
-  //todo: new book form in popout
-  //https://jasonwatmore.com/post/2023/01/03/angular-14-modal-popup-dialog-tutorial-with-example
-  //https://stackoverflow.com/questions/76621643/angular-open-component-in-new-browser-window
-
-
-  //todo: i want 'versino control' form my thoughts/essays on the books
-  //ranking after reads should be unneccessary with versino control
+export class BookListComponent {
 
 }
