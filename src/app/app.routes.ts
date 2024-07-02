ce4889ae579d6f74644bce3344a8b402f5bb8c81
HIschoweRthoughtS <jonasprivat@tutamail.com>
2024-07-02 14:56:19 +0200

    change routes to match previous commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { BookListComponent } from './book-list/book-list.component';
 import { NewBookFormComponent } from './new-book-form/new-book-form.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ReviewTableComponent } from './review-table/review-table.component';
 
 export const routes: Routes = [
     {
@@ -10,12 +12,22 @@ export const routes: Routes = [
     }
     ,{
         path: 'readlist',
-        component: BookListComponent,
+        component: ReviewTableComponent,
         title: 'Bewertung'
+    }
+    ,{
+        path: 'booklist',
+        component: BookListComponent,
+        title: 'My Book Todos'
     }
     ,{
         path: 'newbook',
         component: NewBookFormComponent,
         title: 'New Book'
+    }
+    ,{
+        path: '**', //pagenotfound 404
+        component: PageNotFoundComponent,
+        title: 'Page Not Found'
     }
 ];
