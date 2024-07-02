2d718f4eff5f1f14d364640f454bff8b00a72941
HIschoweRthoughtS <jonasprivat@tutamail.com>
2024-07-02 16:15:14 +0200

    first working add book

--- src/app/new-book-form/new-book-form.component.ts
+++ src/app/new-book-form/new-book-form.component.ts
@@ -1,21 +1,66 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms'
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
+import { RouterLink } from '@angular/router';
+import { Book } from '../shared/interfaces';
+import { MemoryDbService } from '../shared/services/memory-db.service';
 
 @Component({
   selector: 'app-new-book-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterLink,ReactiveFormsModule],
   templateUrl: './new-book-form.component.html',
   styleUrl: './new-book-form.component.scss'
 })
 export class NewBookFormComponent implements OnInit {
+  //this componetn should be displayed in a popout window.
+  //this way the form wont take away any space and you can
+  //start multiple new forms without finfishing any.
+  //on devices without popout (i think smartphones eg)
+  //link to this page with redirect back on finish
   //only the isbn is necessary to find the book, but when no book is found, a new one will be created.
   //the title input will become mandatory
   //class halfoptional -> becomes neccessary under cerain conditions
 
-  constructor(private formBuilder:FormBuilder) {}
+  newBookForm:any;
+
+  constructor(private formBuilder:FormBuilder, private dbService:MemoryDbService) {
+    //noteworthy methods: patchValue, setValidators, updateValueAndValidity
+    this.newBookForm = this.formBuilder.group({
+      isbn : ['', Validators.required],
+      title : ['', Validators.required],
+      extendedTitle : ['', Validators.required],
+      author : ['', Validators.required],
+      publisher : ['', Validators.required],
+      started : ['', Validators.required],
+      finished : ['', Validators.required],
+      quicknote : ['', Validators.required],
+      thoughts : ['', Validators.required],
+      
+    });
+  }
 
   ngOnInit(): void {
-    
+  }
+
+  newBookSubmit(/*?*/) {
+    let tmpForm = this.newBookForm.value;
+    let tmpBook:Book = {
+      isbn: tmpForm.isbn,
+      author: tmpForm.author,
+      title: tmpForm.title,
+      publisher: tmpForm.publisher,
+      pages: 404,
+
+      extended_title: tmpForm.extended_title,
+      thoughts: tmpForm.thoughts,
+
+      more_pages: null,
+      reads: null,
+      rank: null,
+      extra_info: null,
+    };
+    this.dbService.addBook(tmpBook);
+    console.log('test', this.newBookForm.value);
   }
 }
