1691c752820f28ed44ad3218f58fe0ccd1b699d8
HIschoweRthoughtS <jonasprivat@tutamail.com>
2024-07-02 12:09:55 +0200

    basic inmemdb done

---
+++ src/app/shared/db/db.ts
@@ -0,0 +1,35 @@
+import { Book } from "../interfaces";
+
+interface DbStruct {
+    books:Book[]
+}
+
+let db:DbStruct = {
+    "books":[
+        {
+            "isbn":"978-0-141-43947-1",
+            "author":"Marry Shelly",
+            "title":"Frankenstein",
+            "extended_title":"Frankenstein or THE MODERN PROMETHEUS//Edited with an Introduction and Notes by MAURICE HINDLE//REVISED EDITION",
+            "publisher":"PENGUIN CLASSICS",
+            "pages":225,
+            "more_pages":273,
+            "reads":[
+                {
+                    "startDate":"12/2023",
+                    "finishDate":"03/2024",
+                    "quicknote":"Read Again",
+                    "review": {
+                        "content":"already a favourite",
+                        "createdAt":"01.01.1970",
+                        "updatedAt":"01.01.1970"
+                    },
+                    "short_essay":""
+                }     
+            ],
+            "extra_info":"",
+            "thoughts":""
+        }
+    ]
+};
+export { db, DbStruct };
--- src/app/shared/services/memory-db.service.ts
+++ src/app/shared/services/memory-db.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { db, DbStruct } from '../db/db'
+import { Book } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemoryDbService {
+  private inMemDb:DbStruct;
 
-  constructor() { }
+  constructor() {
+    this.inMemDb = db;
+  }
+
+  get Books(): Book[] {
+    return this.inMemDb.books;
+  }
+  
+  set Books(newBooks:DbStruct) {
+    console.log(this.inMemDb);
+    this.inMemDb = newBooks;
+  }
 }
