517515eb596b735e3335d427612c0928c38c9d7a
HIschoweRthoughtS <jonasprivat@tutamail.com>
2024-07-02 12:51:38 +0200

    memdb use observable

--- src/app/shared/services/memory-db.service.ts
+++ src/app/shared/services/memory-db.service.ts
@@ -1,21 +1,31 @@
 import { Injectable } from '@angular/core';
 import { db, DbStruct } from '../db/db'
 import { Book } from '../interfaces';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemoryDbService {
   private inMemDb:DbStruct;
+  private DbBooksSubject:BehaviorSubject<Book[]>;
+  public Books$:Observable<Book[]>;
 
   constructor() {
     this.inMemDb = db;
+    this.DbBooksSubject = new BehaviorSubject<Book[]>(this.inMemDb.books);
+    this.Books$ = this.DbBooksSubject.asObservable();
   }
 
   get Books(): Book[] {
     return this.inMemDb.books;
   }
-  
+
+  addBook(book:Book) {
+    this.inMemDb.books.push(book);
+    this.DbBooksSubject.next(this.Books);
+  }
+
   set Books(newBooks:DbStruct) {
     console.log(this.inMemDb);
     this.inMemDb = newBooks;
