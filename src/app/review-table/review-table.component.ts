d22f71e93d04edf7074963a781c41a8363f3e5cf
HIschoweRthoughtS <jonasprivat@tutamail.com>
2024-07-02 14:50:32 +0200

    generate new components

---
+++ src/app/review-table/review-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './review-table.component.html',
+  styleUrl: './review-table.component.scss'
+})
+export class ReviewTableComponent {
+
+}
