0196a694e76512d63f72a494295dfdfddbaa138d
HIschoweRthoughtS <jonasprivat@tutamail.com>
2024-07-02 14:46:57 +0200

    fix routing in app.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
+import { NewBookFormComponent } from './new-book-form/new-book-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/readlist',
+        pathMatch: 'full'
+    }
+    ,{
+        path: 'readlist',
+        component: BookListComponent,
+        title: 'Bewertung'
+    }
+    ,{
+        path: 'newbook',
+        component: NewBookFormComponent,
+        title: 'New Book'
+    }
+];
