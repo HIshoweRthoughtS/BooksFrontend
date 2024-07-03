b50beceb2534f145b02986cdbcd2c8d9c1d33f5b
HIschoweRthoughtS <jonasprivat@tutamail.com>
2024-07-03 07:52:40 +0200

    add footer to index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { BookListComponent } from './book-list/book-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink,RouterOutlet,BookListComponent],
+  imports: [RouterLink,RouterOutlet,BookListComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
