30ca67ed8d96113594958978ba9b61be3439498f
HIschoweRthoughtS <jonasprivat@tutamail.com>
2024-07-06 13:47:17 +0200

    extend db service

--- src/app/shared/services/memory-db.service.ts
+++ src/app/shared/services/memory-db.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { db, DbStruct } from '../db/db'
-import { Book } from '../interfaces';
+import { Book, Review, ReviewedBook, TodoBook } from '../interfaces';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
@@ -8,15 +8,33 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class MemoryDbService {
   private inMemDb:DbStruct;
+  private DbSubject:BehaviorSubject<DbStruct>;
   private DbBooksSubject:BehaviorSubject<Book[]>;
+  private DbTodosSubject:BehaviorSubject<TodoBook[]>;
+  private DbReadsSubject:BehaviorSubject<ReviewedBook[]>;
 
   constructor() {
     this.inMemDb = db;
+    this.DbSubject = new BehaviorSubject<DbStruct>(this.inMemDb);
     this.DbBooksSubject = new BehaviorSubject<Book[]>(this.inMemDb.books);
+    this.DbTodosSubject = new BehaviorSubject<TodoBook[]>(this.inMemDb.todos);
+    this.DbReadsSubject = new BehaviorSubject<ReviewedBook[]>(this.inMemDb.read);
+  }
+
+  get All(): Observable<DbStruct> {
+    return this.DbSubject.asObservable();
   }
 
   get Books(): Observable<Book[]> {
     return this.DbBooksSubject.asObservable();
+  }
+
+  get Todos(): Observable<TodoBook[]> {
+    return this.DbTodosSubject.asObservable();
+  }
+
+  get Reviews(): Observable<ReviewedBook[]> {
+    return this.DbReadsSubject.asObservable();
   }
 
   addBook(book:Book) {
