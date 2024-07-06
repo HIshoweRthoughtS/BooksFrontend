963b1dea0c45b27c3e9824c65d5a39c86c0190a8
HIschoweRthoughtS <jonasprivat@tutamail.com>
2024-07-06 13:48:16 +0200

    show db at all times for debug purpose; todo: remove this again

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MemoryDbService } from '../shared/services/memory-db.service';
+import { Observable } from 'rxjs';
+import { DbStruct, db } from '../shared/db/db';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule,RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  db$:Observable<DbStruct>;
+
+  constructor(private dbService:MemoryDbService) {
+    this.db$ = dbService.All;
+  }
 
 }
