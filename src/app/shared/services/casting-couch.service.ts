d62a621534de02d461defcf0b2489860c53071da
HIschoweRthoughtS <jonasprivat@tutamail.com>
2024-07-07 17:16:44 +0200

    new casting capability

--- src/app/shared/services/casting-couch.service.ts
+++ src/app/shared/services/casting-couch.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Book, ReviewedBook, TodoBook } from '../interfaces';
+import { Book, BookReadStates, Read, ReviewedBook, TodoBook } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +10,8 @@ export class CastingCouchService {
 
   public toBook(b:TodoBook): Book;
   public toBook(b:ReviewedBook): Book;
-  public toBook(b:ReviewedBook | TodoBook) {
-    const tmpBook = {
+  public toBook(b:ReviewedBook | TodoBook): Book {
+    const tmpBook:Book = {
       isbn:b.isbn,
       author:b.author,
       title:b.title,
@@ -19,9 +19,21 @@ export class CastingCouchService {
       pages:b.pages,
       extended_title:b.extended_title,
       more_pages:b.more_pages,
+      read_state:b.read_state,
       extra_info:b.extra_info,
       thoughts:b.thoughts
     }
     return tmpBook;
+  }
+
+  public getNewReviewedBook(b:Book, read:Read): ReviewedBook;
+  public getNewReviewedBook(b:TodoBook, read:Read): ReviewedBook;
+  public getNewReviewedBook(b:Book | TodoBook, read:Read): ReviewedBook {
+    return {
+      ...b,
+      read_state: BookReadStates.reviewed,
+      reads: [read],
+      rank: -1
+    }
   }
 }
