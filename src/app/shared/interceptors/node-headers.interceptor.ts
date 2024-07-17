439df80a171240227f7fe3b7a88723e8f7a9508d
HIshoweRthoughtS <jonasprivat@tutamail.com>
2024-07-18 00:23:06 +0200

    interceptor to use express session

--- src/app/shared/interceptors/node-headers.interceptor.ts
+++ src/app/shared/interceptors/node-headers.interceptor.ts
@@ -3,9 +3,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const nodeHeadersInterceptor: HttpInterceptorFn = (req, next) => {
   // Clone the request and add the authorization header
   const expressSession = req.clone({
-    setHeaders: {
-      withCredentials: 'true'
-    }
+    withCredentials: true
   });
 
   // Pass the cloned request with the updated header to the next handler
