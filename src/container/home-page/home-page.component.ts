4f063eedf1bfd35b4b3294253ec0add2db694fe3
HIschoweRthoughtS <jonasprivat@tutamail.com>
2024-08-05 19:56:21 +0200

    home message

--- src/container/home-page/home-page.component.ts
+++ src/container/home-page/home-page.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AccountsService } from '../../shared/services/manager/accounts.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'home-page',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent implements OnInit {
   public loginForm:FormGroup<any>; 
+  public logedIn$ = this.accd.logedIn$;
 
   constructor(private readonly accd:AccountsService,private readonly formBuilder:FormBuilder) {
     this.loginForm = formBuilder.group({
